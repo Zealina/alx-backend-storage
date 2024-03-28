645622f5506d1ceb89c932e1865fdd9f30456030
Sebastine Hamilton <hamilsebastine@gmail.com>
2024-03-28 02:45:07 +0100

    Type Annotations

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,20 +1,54 @@
 #!/usr/bin/env python3
-"""Create Redis app"""
+"""Redis module implementation/usage"""
 
 import redis
 from uuid import uuid4
-from typing import Union
+from typing import Optional, Union, Callable
 
 
 class Cache:
-    """Class yo cache stuff"""
-    def __init__(self) -> None:
-        """Create an instance or Redis"""
+    """
+        Cache class.
+    """
+    def __init__(self):
+        """
+            Initialize the cache.
+        """
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """Create a key to store data"""
-        key = str(uuid4())
-        self._redis.set(key, data)
-        return key
+        """
+            Store data in the cache.
+        """
+        randomKey = str(uuid4())
+        self._redis.set(randomKey, data)
+        return randomKey
+
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """
+            Get data from the cache.
+        """
+        value = self._redis.get(key)
+        if fn:
+            value = fn(value)
+        return value
+
+    def get_str(self, key: str) -> str:
+        """
+            Get a string from the cache.
+        """
+        value = self._redis.get(key)
+        return value.decode("utf-8")
+
+    def get_int(self, key: str) -> int:
+        """
+            Get an int from the cache.
+        """
+        value = self._redis.get(key)
+        try:
+            value = int(value.decode("utf-8"))
+        except Exception:
+            value = 0
+        return value
